d2c6bc346935576395fd7716a71737fe7eb55268
Gal1galaxy <110841439+Gal1galaxy@users.noreply.github.com>
2025-05-06 21:37:43 +0800

    Update WordPreview.js

--- frontend/src/components/previews/WordPreview.js
+++ frontend/src/components/previews/WordPreview.js
@@ -1,3 +1,33 @@
+//#################2025.5.6重构代码################
+import React from 'react';
+import { Typography } from 'antd';
+
+const { Paragraph } = Typography;
+
+const WordPreview = ({ data }) => {
+  if (!data || !data.content) {
+    return <div style={{ padding: '20px' }}>没有内容可预览</div>;
+  }
+
+  // 如果 content 是字符串，则按换行符分割成数组
+  const paragraphs = typeof data.content === 'string'
+    ? data.content.split('\n')
+    : data.content;
+
+  return (
+    <div style={{ padding: '20px' }}>
+      {paragraphs.map((paragraph, index) => (
+        <Paragraph key={index}>
+          {paragraph}
+        </Paragraph>
+      ))}
+    </div>
+  );
+};
+
+export default WordPreview;
+
+/*###########初始代码###########
 import React from 'react';
 import { Typography } from 'antd';
 
@@ -16,3 +46,4 @@ const WordPreview = ({ data }) => {
 };
 
 export default WordPreview; 
+###########初始代码###########*/
